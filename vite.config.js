494c2aba89cec5208250ef141ae28b823b255d27
uzma-a <uzmaaasia444@gmail.com>
2024-11-24 12:43:05 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DevMock/', // Set the base to your repository name
   plugins: [react()],
-});
+})
